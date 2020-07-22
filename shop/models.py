e3b5538ea5d1131f972735d2b36130aa6562ca68
mjson1954 <mjson1954@gmail.com>
2020-07-23 02:00:58 +0900

    15강

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Item(models.Model):
     name = models.CharField(max_length=100, validators=[])
@@ -10,3 +11,6 @@ class Item(models.Model):
 
     def __str__(self):
         return f'<{self.pk}> {self.name}'
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
