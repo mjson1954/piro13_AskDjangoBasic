c6f8b75d18b6f9a60b07aa85fdffbc0c6454a76a
mjson1954 <mjson1954@gmail.com>
2020-07-23 10:44:58 +0900

    17강

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
+import logging
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
 def archives_year(request, year):
     return HttpResponse('{}년도에 대한 내용'.format(year))
 
@@ -12,6 +14,7 @@ def item_list(request):
     q = request.GET.get('q', '')
     if q:
         qs.filter(name__icontains=q)
+    logger.debug('query: {}'.format(q))
     return render(request, 'shop/itme_list.html',{
         'item_list': qs,
     })
