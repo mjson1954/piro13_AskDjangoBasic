c932e6ca1df3a126931fd101f5ddc314a4eaf7c5
mjson1954 <mjson1954@gmail.com>
2020-07-23 12:19:53 +0900

    22강

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -37,11 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop',
+    'django.contrib.humanize',
+
     'django_extensions',
-    'blog',
     'debug_toolbar',
     'django_jinja',
+
+    'blog',
+    'shop',
 ]
 
 MIDDLEWARE = [
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Item
 import logging
@@ -17,4 +17,11 @@ def item_list(request):
     logger.debug('query: {}'.format(q))
     return render(request, 'shop/itme_list.html',{
         'item_list': qs,
+        'q': q,
+    })
+
+def item_detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    return render(request, 'shop/item_detail.html',{
+        'item': item,
     })
