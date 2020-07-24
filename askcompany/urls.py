c62cb8d6d70f321ec29c495db0ef88cbd1c2e40f
mjson1954 <mjson1954@gmail.com>
2020-07-24 14:29:29 +0900

    26강

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 from django.conf import settings
+from django.conf.urls.static import static
 
 # def mysum(request, x, y):
 #     result = x + y
@@ -13,6 +14,8 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     # path('mysum/<int:x>/<int:y>/', mysum),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.conf import settings
+from askcompany.utils import uuid_upload_to
 
 # Create your models here.
 class Post(models.Model):
     author_name = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     content = models.TextField()
+    photo = models.ImageField(upload_to=uuid_upload_to)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
