73e76a52e026051d6bbad0690bde3be0a3f8aff5
mjson1954 <mjson1954@gmail.com>
2020-07-24 15:16:04 +0900

    29강

--- shop/urls.py
+++ shop/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, register_converter
+from django.urls import path, re_path, register_converter
 from .converters import FourDigitYearConverter
 from . import views
 
@@ -6,8 +6,11 @@ register_converter(FourDigitYearConverter, 'yyyy')
 
 app_name = 'shop'
 
-urlpatterns=[
+urlpatterns = [
     path('archives/<yyyy:year>/', views.archives_year),
-    path(''. views.item_list),
-    path('<int:pk>/', views.item_detail, name='item'),
+    path('', views.item_list, name='item_list'),
+    path('<int:pk>/', views.item_detail),
+    # re_path(r'^(?P<pk>\d+)/$', views.item_detail),
+    path('item/new/', views.item_new),
+    path('item/<int:pk>/edit/', views.item_edit),
 ]
